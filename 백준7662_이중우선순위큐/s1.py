a71dc673e765ca5275c7b72cc43576947192b137
tmddud <tmddud0103@gmail.com>
2021-10-09 17:33:42 +0900

    add

--- 백준7662_이중우선순위큐/s1.py
+++ 백준7662_이중우선순위큐/s1.py
@@ -7,33 +7,33 @@ TC = int(sys.stdin.readline().lstrip())
 for _ in range(TC):
     heap = []
     maxheap = []
-    ins = 0
-    dele = 0
+    visit = [0]*1000001
     n = int(sys.stdin.readline().lstrip())
-    for _ in range(n):
+    for i in range(n):
         in_de, num = list(map(str, sys.stdin.readline().split()))
         if in_de == 'I':
-            heapq.heappush(heap, int(num))
-            heapq.heappush(maxheap, (-0.1 * int(num), num))
-            ins += 1
+            heapq.heappush(heap, (int(num), i))
+            heapq.heappush(maxheap, (-1 * int(num), i))
+            visit[i] = 1
+        elif num == '1':
+            while maxheap and visit[maxheap[0][1]]==0:
+                heapq.heappop(maxheap)
+            if maxheap:
+                visit[maxheap[0][1]] = 0
+                heapq.heappop(maxheap)
         else:
-            dele += 1
-            if ins <= dele:
-                if ins > 0:
-                    heap = []
-                    maxheap = []
-                    ins = 0
-                dele = 0
-            else:
-                if num == '1':
-                    heapq.heappop(maxheap)
-                else:
-                    heapq.heappop(heap)
-    if ins == 0:
-        print('EMPTY')
-    elif ins - dele == 1:
-        print('{} {}'.format(heap[0], heap[0]))
+            while heap and visit[heap[0][1]] == 0:
+                heapq.heappop(heap)
+            if heap:
+                visit[heap[0][1]] = 0
+                heapq.heappop(heap)
+    while maxheap and visit[maxheap[0][1]] == 0:
+        heapq.heappop(maxheap)
+    while heap and visit[heap[0][1]] == 0:
+        heapq.heappop(heap)
+
     else:
-        print('{} {}'.format(maxheap[0][1], heap[0]))
-
-
+        if maxheap and heap:
+            print('{} {}'.format(-maxheap[0][0], heap[0][0]))
+        else:
+            print('EMPTY')
